d16d8a20662fee7366290eadec0ce772512c65fa
Max-Lev <maxlevtov@gmail.com>
2025-05-02 16:45:02 +0300

    details list paginator

--- src/app/shared/features/details/details.component.ts
+++ src/app/shared/features/details/details.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, effect, inject, Input, OnInit, signal } from "@angular/core";
+import { AfterViewInit, Component, computed, effect, inject, Input, OnInit, QueryList, signal, ViewChild, ViewChildren } from "@angular/core";
 import { GetDetailsService } from "../../../core/providers/get-details.service";
 import { IDetail, IDetailsResponse } from "../../models/details.model";
-import { MatTableModule } from "@angular/material/table";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from "@angular/common";
 import { MatIcon } from "@angular/material/icon";
@@ -14,7 +14,7 @@ import { IsEmptyPipe } from "../../pipes/is-empty.pipe";
 import { DetailsCardComponent } from "../../components/details-card/details-card.component";
 import { LayoutSelectionService } from "../../providers/layout-selection.service";
 import { SelectionService } from "../../providers/selection.service";
-
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 
 @Component({
@@ -26,12 +26,13 @@ import { SelectionService } from "../../providers/selection.service";
     RowComponent,
     NameRowComponent,
     ColorPipe, IsEmptyPipe,
-    DetailsCardComponent
+    DetailsCardComponent,
+    MatPaginatorModule
   ],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, AfterViewInit {
 
   @Input() charactersDetails!: IDetailsResponse;
   getDetailsService = inject(GetDetailsService);
@@ -53,13 +54,43 @@ export class DetailsComponent implements OnInit {
   selectionService = inject(SelectionService);
   selectedViewSignal$ = this.selectionService.selectedViewSignal$;
 
+  dataSource = new MatTableDataSource<{ episode: string, name: string }>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  episodeDataSources: { [id: number]: MatTableDataSource<any> } = {};
+  paginators: { [id: number]: MatPaginator } = {};
+  @ViewChildren('paginator') paginatorsViewChildren!: QueryList<MatPaginator>;
+
   ngOnInit(): void {
     this.expandAllSubTables();
+   this.setEpisodsodeDataSources();
+    console.log(this.episodeDataSources);
+    this.dataSource.paginator = this.paginator;
+    console.log(this.dataSource.paginator, this.paginator);
+  }
+
+  ngAfterViewInit() {
+     this.bindPaginators();
+  }
+  setEpisodsodeDataSources() {
+    for (const character of this.charactersDetails) {
+      const id = +character.character.id;
+      this.episodeDataSources[id] = new MatTableDataSource(character.episodes);
+    }
+  }
+  bindPaginators() {
+    setTimeout(() => {
+      this.paginatorsViewChildren.forEach((paginator, index) => {
+        const id = +this.charactersDetails[index].character.id;
+        this.episodeDataSources[id].paginator = paginator;
+      });
+    });  
   }
 
   expandAllSubTables() {
     this.charactersDetails.forEach((character: IDetail) => {
-      this.expandedRows[+character.character.id] = false; // Remove + to keep as string
+      // this.expandedRows[+character.character.id] = false; // HIDE
+      this.expandedRows[+character.character.id] = true; // SHOW ON INITIAL LOAD
     });
   }
 
@@ -68,10 +99,13 @@ export class DetailsComponent implements OnInit {
   };
 
   toggle(element: IDetail) {
+    this.setEpisodsodeDataSources();
+    this.bindPaginators();
     const id = element.character.id;
     this.expandedRows[+id] = !this.expandedRows[+id];
     // If using plain array, force change detection:
     this.charactersDetails = [...this.charactersDetails];
+
   }
 
   isSelected(row: Character): boolean {
